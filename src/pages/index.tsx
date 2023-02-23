e1db0f7d6916ba5a72b651b3ed18dd84960d7426
Matheus Betim <matheus.betim@synvia.com>
2023-02-22 22:21:16 -0300

    chore: bootstrap the project

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Hello world</h1>
+      </main>
+    </>
+  );
+};
+
+export default Home;
