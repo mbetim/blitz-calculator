4c8059cc788610ed15b3b37908c4d0c5cbaf4033
Matheus Betim <mbetim47@gmail.com>
2023-03-02 22:32:52 -0300

    chore: install and set up react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import { ChakraProvider, extendTheme, type Theme } from "@chakra-ui/react";
 import { type AppType } from "next/dist/shared/lib/utils";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ChakraProvider theme={extendTheme({ config: { initialColorMode: "dark" } } as Theme)}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={extendTheme({ config: { initialColorMode: "dark" } } as Theme)}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 };
 
