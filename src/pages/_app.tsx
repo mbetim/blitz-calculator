5b9bc53f3ea111a7823d966ce7cbf7f0974ba6ba
Matheus Betim <mbetim47@gmail.com>
2023-03-07 20:50:15 -0300

    feat: add a default title for every page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider, extendTheme, type Theme } from "@chakra-ui/react";
 import { type AppType } from "next/dist/shared/lib/utils";
+import Head from "next/head";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
@@ -7,6 +8,10 @@ const queryClient = new QueryClient();
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <QueryClientProvider client={queryClient}>
+      <Head>
+        <title>Blitz calculator</title>
+      </Head>
+
       <ChakraProvider theme={extendTheme({ config: { initialColorMode: "dark" } } as Theme)}>
         <Component {...pageProps} />
       </ChakraProvider>
